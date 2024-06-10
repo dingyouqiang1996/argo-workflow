1136b509cf833f1cf8cad003a7ad35198aab025d
Garett MacGowan <garett.macgowan@gmail.com>
2024-06-09 23:49:53 -0400

    test: test fixes
    Signed-off-by: Garett MacGowan <garett.macgowan@gmail.com>

--- test/e2e/workflow_test.go
+++ test/e2e/workflow_test.go
@@ -65,7 +65,7 @@ metadata:
   generateName: get-resources-via-script-template-
   namespace: argo
 spec:
-  serviceAccountName: default
+  serviceAccountName: argo
   automountServiceAccountToken: false
   executor:
     serviceAccountName: default
